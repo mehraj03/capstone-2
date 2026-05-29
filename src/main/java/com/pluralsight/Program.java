cf46b4c8efcf02cc6055767d556088edcfa1bfc2
mehraj03 <mehrajshafat@gmail.com>
2026-05-29 05:12:55 -0400

    launch DELI-cious app through UserInterface

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -2,12 +2,9 @@ package com.pluralsight;
 
 public class Program {
     public static void main(String[] args) {
-       Order order = new Order();
+      UserInterface ui = new UserInterface();
+              ui.displayHomeScreen();
 
-       order.addItem(new Drink("medium"));
-       order.addItem(new Drink("large"));
-       order.addItem(new Chips("BBQ"));
 
-       System.out.println(order.getOrderDetails());
     }
 }
